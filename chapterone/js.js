20ebc2e01728a66a0991bcf2fe4bd769027039ea
XuanHien <thanlong103@gmail.com>
2023-03-19 00:25:41 +0700

    nay code string

--- chapterone/js.js
+++ chapterone/js.js
@@ -1,1 +1,21 @@
-console.log("success");
+// data type
+// let cần khai bao trước khi sài, ó thể thay đổi giá trị của nó
+
+// let x;
+// x = 10;
+// x = 20;
+// console.log(x);
+
+// const cần được gán giá trị khi khai báo, và không thể thay đổi giá trị của biến
+
+// const y = 10;
+// // y = 20;
+// console.log(y);
+
+// string được cộng từ trái sang phải của từng giá trị
+
+let str1 = 9 + 10 + "hello";
+console.log(str1); //result: 19hello
+
+let str2 = "hello" + 9 + 10;
+console.log(str2); //result: hello910
